b61ddd264a9a6dce3ffcc5acb1ce1dd4b53d6dba
Kshitij-Tidke <ktidke005@gmail.com>
2025-02-19 19:13:48 +0530

    1

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://your-backend.vercel.app/api/v1/task"; // Update with backend URL
+const API_URL = process.env.API_URL;
 
 export const getTasks = async () => {
   const response = await axios.get(API_URL);
